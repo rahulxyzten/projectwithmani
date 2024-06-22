d7d7667a40bdd4c52ad0fe5cce017199aff3218b
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2024-06-23 00:51:57 +0530

    Added Tutorial Page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,26 +5,6 @@ const page = () => {
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <Feed />
-      {/* {resourcesPlaylist.map((item: any) => (
-        <section
-          key={item._id}
-          className="flex-center mt-6 w-full flex-col sm:mt-20"
-        >
-          <h1 className="heading3 self-start text-white-800">{item.title}</h1>
-          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-            {item.resources.map((resource: any) => (
-              <ResourceCard
-                key={resource._id}
-                title={resource.title}
-                id={resource._id}
-                image={resource.image}
-                downloadNumber={resource.views}
-                downloadLink={resource.downloadLink}
-              />
-            ))}
-          </div>
-        </section>
-      ))} */}
     </main>
   );
 };
