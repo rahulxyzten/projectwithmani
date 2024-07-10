5fc457c1bebbf781c57176de8aef3fd47325c46e
rahulXten <beherarahul101@gmail.com>
2024-07-10 13:06:49 +0530

    Minor changes III

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useEffect, useState, Suspense } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import GalleryCard from "@/components/GalleryCard";
 
 const page = () => {
+  const searchParams = useSearchParams();
   const [posts, setPosts] = useState<any[]>([]);
   const [visibleCount, setVisibleCount] = useState(8);
 
@@ -16,45 +18,43 @@ const page = () => {
     };
 
     fetchProjects();
-  }, []);
+  }, [searchParams]);
 
   const handleLoadMore = () => {
     setVisibleCount((prevCount) => prevCount + 8);
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <section className=" flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
-        <div className=" pt-[50px] w-full flex flex-col items-center bg-black-100 text-white-800 min-h-screen">
-          <h1 className="text-4xl font-bold my-8 text-center">
-            This projects are made by creators who learnt from my Tutorials
-          </h1>
-          <Link href="https://forms.gle/your-google-form-link" passHref>
-            <button className="bg-purple hover:bg-pink translation duration-500 text-white font-bold py-2 px-4 rounded mb-8 active:scale-95">
-              Upload your Project Image
-            </button>
-          </Link>
-          <div className="flex flex-wrap justify-center mt-4 gap-4">
-            {posts.slice(0, visibleCount).map((post, index) => (
-              <GalleryCard
-                key={index}
-                title={post.title}
-                username={post.username}
-                image={post.cover.url}
-              />
-            ))}
-          </div>
-          {visibleCount < posts.length && (
-            <button
-              onClick={handleLoadMore}
-              className="bg-purple hover:bg-pink translation duration-500 text-white font-bold py-2 px-4 rounded my-8 active:scale-95"
-            >
-              Load More
-            </button>
-          )}
+    <section className=" flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <div className=" pt-[50px] w-full flex flex-col items-center bg-black-100 text-white-800 min-h-screen">
+        <h1 className="text-4xl font-bold my-8 text-center">
+          This projects are made by creators who learnt from my Tutorials
+        </h1>
+        <Link href="https://forms.gle/your-google-form-link" passHref>
+          <button className="bg-purple hover:bg-pink translation duration-500 text-white font-bold py-2 px-4 rounded mb-8 active:scale-95">
+            Upload your Project Image
+          </button>
+        </Link>
+        <div className="flex flex-wrap justify-center mt-4 gap-4">
+          {posts.slice(0, visibleCount).map((post, index) => (
+            <GalleryCard
+              key={index}
+              title={post.title}
+              username={post.username}
+              image={post.cover.url}
+            />
+          ))}
         </div>
-      </section>
-    </Suspense>
+        {visibleCount < posts.length && (
+          <button
+            onClick={handleLoadMore}
+            className="bg-purple hover:bg-pink translation duration-500 text-white font-bold py-2 px-4 rounded my-8 active:scale-95"
+          >
+            Load More
+          </button>
+        )}
+      </div>
+    </section>
   );
 };
 
